f76195578dd6e257ef5f93c4ef82db3064aafb99
srivatsa.mr <sm222@ic.ac.uk>
2022-10-18 11:56:44 +0100

    Added constants.py and test_constants.py

---
+++ simple_functions/constants.py
@@ -0,0 +1,17 @@
+from functools import cache
+
+from numpy import sqrt
+
+from simple_functions.functions1 import factorial
+
+__all__ = ['pi']
+
+
+def pi(terms=1):
+    return 1./(2.*sqrt(2.)/9801.*rsum(terms))
+
+
+@cache
+def rsum(n):
+    t = factorial(4*n)*(1103+26390*n)/(factorial(n)**4*396**(4*n))
+    return t + rsum(n-1) if n else t
