fb41be534ef57165fce8810149f5637175aaca77
acse-d85ab24e <csy15@ic.ac.uk>
2022-10-18 11:31:19 +0100

    reformatted script

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,6 +1,6 @@
 from functools import cache
 
-__all__ = ['my_sum', 'factorial']
+__all__ = ["my_sum", "factorial"]
 
 
 def my_sum(iterable):
@@ -12,4 +12,4 @@ def my_sum(iterable):
 
 @cache
 def factorial(n):
-    return n * factorial(n-1) if n else 1
+    return n * factorial(n - 1) if n else 1
