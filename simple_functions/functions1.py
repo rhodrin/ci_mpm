5b50c773a44851dbc7a57d4072b7696c50b626aa
acse-b99192e1 <michael.tsesmelis22@ic.ac.uk>
2022-10-18 11:28:21 +0100

    add factorial function

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,5 +1,6 @@
-# import numpy as np
-__all__ = ['my_sum']
+from functools import cache
+
+__all__ = ['my_sum', 'factorial']
 
 
 def my_sum(iterable):
@@ -7,3 +8,7 @@ def my_sum(iterable):
     for i in iterable:
         tot += i
     return tot
+
+@cache
+def factorial(n):
+    return n * factorial(n-1) if n else 1
