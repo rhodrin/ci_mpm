cd08138c2d3752879edd622724008a64f911141b
rhodrin <rhodri.nelson@gmail.com>
2024-10-14 16:04:47 +0100

    MPM 2024 updates.

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='simple_functions',
-      use_scm_version=True,
-      setup_requires=['setuptools_scm'],
-      description="Environment for playing with CI.",
-      long_descritpion="""Playing with CI.""",
-      url='https://github.com/rhodrin/ci_mpm',
-      author="Imperial College London",
-      author_email='rhodri.nelson@imperial.ac.uk',
-      packages=['simple_functions'])
+setup(
+    name='simple_functions',
+    version='0.1.0',  # You can use any version you prefer
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
